b27b5c040c54f92298a9b2ef0e8ffa83ca7f9075
Pablirry <pablo.lopez02@csioviedo.com>
2024-01-22 10:04:41 +0100

    .

---
+++ Ejemplos/src/Ejemplo1/PruebaAlumno.java
@@ -0,0 +1,14 @@
+package Ejemplo1;
+
+public class PruebaAlumno {
+
+	public static void main(String[] args) {
+		
+		Alumno a1 = new Alumno("11111111K","pepe",15);
+		a1.mostrarDatos();
+		Alumno a2 = new Alumno("22222222L","ana",17);
+		a2.mostrarDatos();
+		
+	}
+
+}

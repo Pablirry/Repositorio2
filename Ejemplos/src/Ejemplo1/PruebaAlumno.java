297f423de593c1390ac86608371ddd800f836b7b
Pablirry <pablo.lopez02@csioviedo.com>
2024-01-22 13:54:02 +0100

    .

--- Ejemplos/src/Ejemplo1/PruebaAlumno.java
+++ Ejemplos/src/Ejemplo1/PruebaAlumno.java
@@ -5,11 +5,18 @@ public class PruebaAlumno {
 	public static void main(String[] args) {
 		
 		Alumno a1 = new Alumno("11111111K","pepe",15);
-		String n=a1.getNombre();
-		System.out.println("Nombre Alumno: " +n);
+		String n1=a1.getNombre();
+		System.out.println("Nombre Alumno: " +n1);
 		a1.setNombre("Juan");
-		n=a1.getNombre();
-		System.out.println("Nombre Alumno: " +n);
+		n1=a1.getNombre();
+		System.out.println("Nombre Alumno: " +n1);
+
+		Alumno a2 = new Alumno("11111111L","pedro",18);
+		String n2 = a2.getNombre();
+		System.out.println("Nombre Alumno: " +n2);
+		a2.setNombre("Luis");
+		n2 = a2.getNombre();
+		System.out.println("Nombre Alumno: " +n2);
 		
 
 		

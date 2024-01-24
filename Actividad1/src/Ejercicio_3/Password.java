8d09de9651704c463bbca93f3e458a431a04a2f1
Pablo Lopez <pablolopezruiz1@gmail.com>
2024-01-24 08:53:54 +0100

    .

--- Actividad1/src/Ejercicio_3/Password.java
+++ Actividad1/src/Ejercicio_3/Password.java
@@ -2,4 +2,21 @@ package Ejercicio_3;
 
 public class Password {
 
+    /*
+     * Haz una clase llamada Password que siga las siguientes condiciones:
+     * Que tenga los atributos longitud y contraseña . Por defecto, la longitud
+     * sera de 8.
+     * Los constructores serán los siguiente:
+     * o Un constructor por defecto.
+     * o Un constructor con la longitud que nosotros le pasemos. Generara una
+     * contraseña aleatoria con esa longitud.
+     * Los métodos que implementa serán:
+     * • esFuerte(): devuelve un booleano si es fuerte o no, para que sea fuerte
+     * debe tener mas de 2 mayúsculas, mas de 1 minúscula y mas de 5 números.
+     * • generarPassword(): genera la contraseña del objeto con la longitud que
+     * tenga.
+     * • Método get para contraseña y longitud.
+     * • Método set para longitud.
+     */
+
 }
--- Actividad1/src/Ejercicio_3/PruebaPassword.java
+++ Actividad1/src/Ejercicio_3/PruebaPassword.java
@@ -3,7 +3,18 @@ package Ejercicio_3;
 public class PruebaPassword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * • Crea un array de Passwords con el tamaño que tu le indiques por teclado.
+		 * • Crea un bucle que cree un objeto para cada posición del array.
+		 * • Indica también por teclado la longitud de los Passwords (antes de bucle).
+		 * • Crea otro array de booleanos donde se almacene si el password del array de
+		 * Password es o no fuerte (usa el bucle anterior).
+		 * • Al final, muestra la contraseña y si es o no fuerte (usa el bucle
+		 * anterior). Usa este simple formato:
+		 * o contraseña1 valor_booleano1
+		 * o contraseña2 valor_bololeano2
+		 */
 
 	}
 

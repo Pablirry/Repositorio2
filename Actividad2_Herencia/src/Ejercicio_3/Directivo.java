2f587b26328fd72c12003dceafb416c100c905cd
Pablo Lopez <pablolopezruiz1@gmail.com>
2024-02-20 10:32:20 +0100

    Ejercicio 3

---
+++ Actividad2_Herencia/src/Ejercicio_3/Directivo.java
@@ -0,0 +1,15 @@
+package Ejercicio_3;
+
+public class Directivo extends Empleado {
+
+    public Directivo(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " --> Directivo";
+    }
+
+    
+}
---
+++ Actividad2_Herencia/src/Ejercicio_3/Tecnico.java
@@ -0,0 +1,14 @@
+package Ejercicio_3;
+
+public class Tecnico extends Operario {
+
+    public Tecnico (String nombre){
+        super(nombre);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " --> Tecnico";
+    }
+
+}
